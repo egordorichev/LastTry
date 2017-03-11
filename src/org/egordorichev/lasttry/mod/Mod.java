73ab0f9ec25b69d91ad481d3456a7e28b25c0ce8
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-11 17:46:58 +0300

    Update Mod.java

--- src/org/egordorichev/lasttry/mod/Mod.java
+++ src/org/egordorichev/lasttry/mod/Mod.java
@@ -2,22 +2,10 @@ package org.egordorichev.lasstry.mod;
 
 public class Mod {
 	protected String name;
-	protected boolead loaded;
 	
 	public Mod(String name) {
 		this.name = name;
 		this.loaded = false;
-	}
-	
-	public void load() {
-		if(this.loaded) {
-			// TODO: throw a error
-		}
-		
-		// TODO
-		
-		this.onLoad();
-		this.loaded = true;
 	}
 	
 	protected onLoad() {
