ce388a1e473bd25e70a9fa133d9407d9ad094684
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-11 17:48:35 +0300

    Update ModLoader.java

--- src/org/egordorichev/lasttry/mod/ModLoader.java
+++ src/org/egordorichev/lasttry/mod/ModLoader.java
@@ -1,5 +1,36 @@
 package org.egordorichev.lasttry.mod;
 
 public class ModLoader {
-	// TODO
+	private ArrayList<Mod> mods;
+	
+	public ModLoader() {
+		mods = new ArrayList<>();
+	}
+	
+	public Mod loadMod(String name) {
+		throw new NotImplementedException();
+	}
+	
+	public void unloadMod(String name) {
+		this.unloadMod(this.getMod(name));
+	}
+	
+	public void unloadMod(Mod mod) throws ModException {
+		if(mod == null) {
+			throw new ModException("Mod is null");
+		}
+		
+		mod.onUnload();
+		throw new NotImplementedException();
+	}
+	
+	public Mod getMod(String name) {
+		for(Mod mod : this.mods) {
+			if(mod.getName().equals(name)) {
+				return mod;
+			}
+		}
+		
+		return null;
+	}
 }
