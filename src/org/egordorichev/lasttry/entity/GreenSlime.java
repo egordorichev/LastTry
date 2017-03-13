78306ec6f285085227eedf404a5dc943c84333d5
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-13 19:40:10 +0300

    Update Green Slime animation

--- src/org/egordorichev/lasttry/entity/GreenSlime.java
+++ src/org/egordorichev/lasttry/entity/GreenSlime.java
@@ -8,10 +8,33 @@ public class GreenSlime extends Enemy {
 		super("Green Slime", 28, 0, 12);
 
 		this.image = Assets.greenSlimeTexture;
-	//	this.animation.addFrame(this.image.getSubImage(0, 0, 32, 24), 300);
-	//	this.animation.addFrame(this.image.getSubImage(32, 0, 30, 24), 300);
+
+
+		Animation jumpAnimation = new Animation();
+
+		jumpAnimation.addFrame(this.image.getSubImage(32, 0, 30, 24), 1);
+		jumpAnimation.setLooping(false);
+
+		Animation idleAnimation = new Animation();
+
+		idleAnimation.addFrame(this.image.getSubImage(0, 0, 30, 24), 300);
+		idleAnimation.addFrame(this.image.getSubImage(32, 0, 30, 24), 300);
+
+		this.animations[State.JUMPING.getId()] = jumpAnimation;
+		this.animations[State.IDLE.getId()] = idleAnimation;
+		this.animations[State.DEAD.getId()] = null;
+		this.animations[State.FALLING.getId()] = jumpAnimation;
+		this.animations[State.FLYING.getId()] = null;
+
 
 		this.rect.width = 32;
 		this.rect.height = 24;
+	}
+
+	@Override
+	public void updateAI() {
+		super.updateAI();
+
+		// TODO: bounce and jump
 	}
 }
