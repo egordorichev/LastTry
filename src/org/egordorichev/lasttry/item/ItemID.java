e82ffe7eb481a8a3236acbe179bd6c7d00444c63
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-14 10:56:06 +0300

    Create ItemID.java

---
+++ src/org/egordorichev/lasttry/item/ItemID.java
@@ -0,0 +1,7 @@
+package org.egordorichev.lasttry.item;
+
+public class ItemID {
+	public final static int dirtBlock = 1;
+	public final static int dirtWall = 2;
+	public final static int grassBlock = 3;
+}
