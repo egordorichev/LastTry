78c0ce353f6e9bc6e529e30cf7cd100fe64d717a
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-14 08:19:26 +0300

    Update LastTry.java

--- src/org/egordorichev/lasttry/LastTry.java
+++ src/org/egordorichev/lasttry/LastTry.java
@@ -3,8 +3,6 @@ package org.egordorichev.lasttry;
 import org.egordorichev.lasttry.entity.Player;
 import org.egordorichev.lasttry.ui.UiManager;
 import org.egordorichev.lasttry.world.World;
-import org.keplerproject.luajava.LuaState;
-import org.keplerproject.luajava.LuaStateFactory;
 import org.egordorichev.lasttry.mod.*;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
@@ -19,7 +17,6 @@ public class LastTry extends StateBasedGame {
 	public static World world;
 	public static Player player;
 	public static Camera camera;
-	public static LuaState lua;
 	public static ModLoader modLoader;
 	public static UiManager ui;
 	public static AppGameContainer app;
@@ -56,11 +53,6 @@ public class LastTry extends StateBasedGame {
 		graphics = gameContainer.getGraphics();
 
 		ui = new UiManager();
-
-
-		// Commented out due to issues on windows loading dependencies
-		//lua = LuaStateFactory.newLuaState();
-		//lua.openLibs();
 
 		graphics.setBackground(new Color(129, 207, 224));
 
