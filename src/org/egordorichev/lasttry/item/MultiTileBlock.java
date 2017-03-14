a4cc239e2706be5c656f3926b4470faad60cca3e
Dorichev Egor <egordorichev@gmail.com>
2017-03-14 18:08:33 +0300

    Adding multi-tile block

---
+++ src/org/egordorichev/lasttry/item/MultiTileBlock.java
@@ -0,0 +1,29 @@
+package org.egordorichev.lasttry.item;
+
+import org.egordorichev.lasttry.world.tile.TileData;
+import org.newdawn.slick.Image;
+
+public class MultiTileBlock extends Block {
+	protected int width;
+	protected int height;
+
+	public MultiTileBlock(int id, String name, boolean solid, Image tiles, int gridWidth, int gridHeight) {
+		super(id, name, solid, tiles);
+
+		this.width = gridWidth;
+		this.height = gridHeight;
+	}
+
+	@Override
+	public void renderBlock(TileData data, int x, int y) {
+		// TODO
+	}
+
+	public int getGridWidth() {
+		return this.width;
+	}
+
+	public int getGridHeight() {
+		return this.height;
+	}
+}
