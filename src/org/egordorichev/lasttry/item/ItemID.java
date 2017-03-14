f2f63fa5a7b187c283ddd8bc3f57dc03cd1b5249
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-14 10:57:58 +0300

    Update ItemID.java

--- src/org/egordorichev/lasttry/item/ItemID.java
+++ src/org/egordorichev/lasttry/item/ItemID.java
@@ -1,6 +1,7 @@
 package org.egordorichev.lasttry.item;
 
 public class ItemID {
+	public final static int none = 0;
 	public final static int dirtBlock = 1;
 	public final static int dirtWall = 2;
 	public final static int grassBlock = 3;
