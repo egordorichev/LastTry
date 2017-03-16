9a1c2e771c33635163b2b129d3e3ed85e405fc40
Dorichev Egor <egordorichev@gmail.com>
2017-03-16 14:55:50 +0300

    Working under modifiers

--- src/main/java/org/egordorichev/lasttry/item/Modifier.java
+++ src/main/java/org/egordorichev/lasttry/item/Modifier.java
@@ -47,8 +47,30 @@ public class Modifier {
 	// Universal
 
 	public static final Modifier keen = new Modifier("Keen", Type.UNIVERSAL, new Stat(Stat.Type.CRITICAL_STRIKE_CHANCE, +3));
-	public static final Modifier superior = new Modifier("Superior", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +10), new Stat(Stat.Type.CRITICAL_STRIKE_CHANCE, +3), new Stat(Stat.Type.KNOCKBACK, +10));
-	public static final Modifier foreceful = new Modifier("Foreceful", Type.UNIVERSAL, new Stat(Stat.Type.KNOCKBACK, +15));
+	public static final Modifier superior = new Modifier("Superior", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +10),
+		new Stat(Stat.Type.CRITICAL_STRIKE_CHANCE, +3), new Stat(Stat.Type.KNOCKBACK, +10));
+
+	public static final Modifier forceful = new Modifier("Forceful", Type.UNIVERSAL, new Stat(Stat.Type.KNOCKBACK, +15));
+	public static final Modifier broken = new Modifier("Broken", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, -30),
+		new Stat(Stat.Type.KNOCKBACK, -20));
+
+	public static final Modifier damaged = new Modifier("Damaged", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, -15));
+	public static final Modifier shoddy = new Modifier("Shoddy", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, -10),
+		new Stat(Stat.Type.KNOCKBACK, -15));
+
+	public static final Modifier hurtful = new Modifier("Hurtful", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +10));
+	public static final Modifier strong = new Modifier("Strong", Type.UNIVERSAL, new Stat(Stat.Type.KNOCKBACK, +15));
+	public static final Modifier unpleasant = new Modifier("Unpleasant", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +5),
+		new Stat(Stat.Type.KNOCKBACK, +15));
+
+	public static final Modifier weak = new Modifier("Weak", Type.UNIVERSAL, new Stat(Stat.Type.KNOCKBACK, -10));
+	public static final Modifier ruthless = new Modifier("Ruthless", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +18),
+		new Stat(Stat.Type.KNOCKBACK, -10));
+
+	public static final Modifier demonic = new Modifier("Demonic", Type.UNIVERSAL, new Stat(Stat.Type.DAMAGE, +15),
+			new Stat(Stat.Type.CRITICAL_STRIKE_CHANCE, +5));
+
+	public static final Modifier zealous = new Modifier("Zealous", Type.UNIVERSAL, new Stat(Stat.Type.CRITICAL_STRIKE_CHANCE, +5));
 
 	// Tools
 	//
@@ -56,7 +78,10 @@ public class Modifier {
 	public enum Type {
 		ACCESSORY,
 		UNIVERSAL,
-		TOOL
+		COMMON,
+		MELEE,
+		RANGED,
+		MAGIC
 	}
 
 	protected String name;
