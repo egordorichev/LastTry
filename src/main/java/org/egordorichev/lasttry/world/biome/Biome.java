2cace0518fcea11b7d54050e284b81b59f0a6c01
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-16 20:26:11 +0300

    Create Biome.java

---
+++ src/main/java/org/egordorichev/lasttry/world/biome/Biome.java
@@ -0,0 +1,14 @@
+package org.egordorichev.lasttry.world.biome;
+
+public class Biome {
+	protected String name;
+	// TODO: protected Image[] backgrounds;
+	
+	public Biome(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
