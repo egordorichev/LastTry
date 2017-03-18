1745e06992dc203228b525bf800fcf1ab7db0826
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-18 20:13:59 +0300

    Update World.java

--- src/main/java/org/egordorichev/lasttry/world/World.java
+++ src/main/java/org/egordorichev/lasttry/world/World.java
@@ -558,11 +558,11 @@ public class World {
 						}
 					}
 				}
-				if(totalEvilBlocks > 200) {
+				if(totalEvilBlocks >= 200) {
 					LastTry.world.currentBiome = (LastTry.world.getEvilType() == EvilType.CORRUPTION) ? Biome.corruption : Biome.crimson;
-				} else if(totalEvilDesertBlocks > 1000) {
+				} else if(totalEvilDesertBlocks >= 1000) {
 					LastTry.world.currentBiome = (LastTry.world.getEvilType() == EvilType.CORRUPTION) ? Biome.corruptDesert : Biome.crimsonDesert;
-				} else if(totalDesertBlocks > 1000) {
+				} else if(totalDesertBlocks >= 1000) {
 					LastTry.world.currentBiome = Biome.desert;
 				} else {
 					LastTry.world.currentBiome = Biome.forest;
