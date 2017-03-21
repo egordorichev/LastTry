266ec6431efea6d471ce834d8fe1a68feb899aa3
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-21 11:07:31 +0300

    Update Ammo.java

--- src/main/java/org/egordorichev/lasttry/item/items/Ammo.java
+++ src/main/java/org/egordorichev/lasttry/item/items/Ammo.java
@@ -4,9 +4,52 @@ import org.egordorichev.lasttry.item.Item;
 import org.newdawn.slick.Image;
 
 public class Ammo extends Item {
-	public Ammo(int id, String name, Image texture) {
+	public enum Type {
+		BULLET,
+		ARROW,
+		ROCKET,
+		DART,
+		SOLUTION, // Used by some tools
+		BAIT,
+		WIRE,
+		OTHER
+	}
+	
+	/**
+	 * Ammo damage
+	 */
+	protected int damage;
+	
+	/**
+	 * Ammo type
+	 */
+	protected Type type;
+	
+	public Ammo(int id, String name, int damage, Type type, Image texture) {
 		super(id, name, texture);
+		
+		this.damage = damage;
+		this.type = type;
 	}
 
-	// TODO
+	/**
+	 * @return if ammo can damage
+	 */
+	public boolean hasDamage() {
+		return this.damage > 0;
+	}
+	
+	/**
+	 * @return ammo damage
+	 */
+	public int getDamage() {
+		return this.damage;
+	}
+	
+	/**
+	 * @return ammo type
+	 */
+	public Type getType() {
+		return this.type;
+	}
 }
