ec200bb8633013f011a7fa1a48c86ec0da4c44b2
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-21 10:59:00 +0300

    :memo: Adding docs to Armor class and updating it

--- src/main/java/org/egordorichev/lasttry/item/items/Armor.java
+++ src/main/java/org/egordorichev/lasttry/item/items/Armor.java
@@ -4,9 +4,40 @@ import org.egordorichev.lasttry.item.Item;
 import org.newdawn.slick.Image;
 
 public class Armor extends Item {
-	public Armor(int id, String name, Image texture) {
-		super(id, name, texture);
+	public enum Slot {
+		HEAD,
+		BODY,
+		LEGS
 	}
-
-	// TODO
+	
+	/**
+	 * Defense, provided by this armor piece
+	 */
+	protected int defense;
+	
+	/**
+	 * In witch slot this armor can be placed
+	 */
+	protected Slot slot;
+	
+	public Armor(int id, String name, int defense, Slot slot, Image texture) {
+		super(id, name, texture);
+	
+		this.defense = defense;
+		this.slot = slot;
+	}
+	
+	/**
+	 * @return armor defense
+	 */
+	public int getDefense() {
+		return this.defense;
+	}
+	
+	/**
+	 * @return armor slot
+	 */
+	public Slot getSlot() {
+		return this.slot;
+	}
 }
