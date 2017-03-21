436028a724054df1123c01da3f2fd3d9552678b5
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-21 10:46:28 +0300

    :memo: Adding docs for ModAPI

--- src/main/java/org/egordorichev/lasttry/mod/ModAPI.java
+++ src/main/java/org/egordorichev/lasttry/mod/ModAPI.java
@@ -7,27 +7,54 @@ import org.egordorichev.lasttry.world.World;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.KeyListener;
 
-public class ModAPI { // TODO: add more
+public class ModAPI {
+	/**
+	 * @return reference to client's player
+	 */
 	public static Player getPlayer() {
 		return LastTry.player;
 	}
 
+	/**
+	 * @return reference to current world or null, if it is not loaded yet
+	 */
 	public static World getWorld() {
 		return LastTry.world;
 	}
 
+	/**
+	 * @param id enemy id (listed in EntityID class)
+	 * @return new Enemy with given id, or null, if it is not found
+	 */
 	public static Enemy createEnemy(int id) {
 		return Enemy.create(id);
 	}
 	
+	/**
+	 * @param id enemy id (listed in EntityID class)
+	 * @param x enemy spawn X coordinate
+	 * @param y enemy spawn Y coordinate
+	 * @return new Enemy with given id, spawned on X, Y
+	 */
 	public static Enemy spawnEnemy(int id, int x, int y) {
 		return LastTry.world.spawnEnemy(id, x, y);
 	}
 	
+	/**
+	 * Adds given enemy class to the enemy register
+	 * Now it can be accesed with createEnemy(id) or spawnEnemy(id, x, y)
+	 * @param id with witch id the enemy will be defined
+	 * @param enemyClass enemy class
+	 */
 	public static void defineEnemy(int id, Class<? extends Enemy> enemyClass) {
 		Enemy.define(id, enemyClass);
 	}
 
+	/**
+	 * Adds new key listner to the game input
+	 * @param key Slick2d Input.Key
+	 * @param callable will be called, when key is pressed
+	 */
 	public static void addKeyBinding(int key, Callable callable) {
 		LastTry.input.addKeyListener(new KeyListener() {
 			@Override
