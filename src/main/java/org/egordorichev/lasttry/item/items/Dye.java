263dc5df17f996422d537533c87531fdf6ffe5dc
Dorichev Egor <egordorichev@gmail.com>
2017-03-21 11:41:12 +0300

    Updated

--- src/main/java/org/egordorichev/lasttry/item/items/Dye.java
+++ src/main/java/org/egordorichev/lasttry/item/items/Dye.java
@@ -2,14 +2,8 @@ package org.egordorichev.lasttry.item.items;
 
 import org.egordorichev.lasttry.item.Item;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.opengl.shader.ShaderProgram;
 
 public class Dye extends Item {
-	/**
-	 * Shader, applyed on other items
-	 */
-	protected ShaderProgram shader;
-	
 	public Dye(int id, String name, Image texture) {
 		super(id, name, texture);
 	}
