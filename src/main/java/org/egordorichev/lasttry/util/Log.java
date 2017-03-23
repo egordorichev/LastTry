da76a158e1fcae665545d4f0fbde040b6bf87996
Dorichev Egor <egordorichev@gmail.com>
2017-03-23 15:54:42 +0300

    Adding colors to Log class

--- src/main/java/org/egordorichev/lasttry/util/Log.java
+++ src/main/java/org/egordorichev/lasttry/util/Log.java
@@ -1,6 +1,16 @@
 package org.egordorichev.lasttry.util;
 
 public class Log {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
 	private boolean enabled;
 
 	public Log() {
@@ -8,31 +18,47 @@ public class Log {
 	}
 
 	public void debug(String message) {
-		System.out.print("DEBUG ");
+		if (!this.enabled) {
+			return;
+		}
+
+		System.out.print(ANSI_GREEN + "DEBUG ");
 		this.printPath(Thread.currentThread().getStackTrace());
-		System.out.println(": " + message);
+		System.out.println(": " + message + ANSI_RESET);
 	}
 
 	public void warn(String message) {
-		System.out.print("WARN ");
+		if (!this.enabled) {
+			return;
+		}
+
+		System.out.print(ANSI_RED + "WARN ");
 		this.printPath(Thread.currentThread().getStackTrace());
-		System.out.println(": " + message);
+		System.out.println(": " + message + ANSI_RESET);
 	}
 
 	public void error(String message) {
-		System.out.print("ERROR ");
+		if (!this.enabled) {
+			return;
+		}
+
+		System.out.print(ANSI_RED + "ERROR ");
 		this.printPath(Thread.currentThread().getStackTrace());
-		System.out.println(": " + message);
+		System.out.println(": " + message + ANSI_RESET);
 	}
 
 	public void info(String message) {
-		System.out.print("INFO ");
+		if (!this.enabled) {
+			return;
+		}
+
+		System.out.print(ANSI_BLUE + "INFO ");
 		this.printPath(Thread.currentThread().getStackTrace());
-		System.out.println(": " + message);
+		System.out.println(": " + message + ANSI_RESET);
 	}
 
 	private void printPath(StackTraceElement[] stackTraceElements) {
-		System.out.print(stackTraceElements[1].getClassName());
+		System.out.print(stackTraceElements[3].getClassName());
 	}
 
 	public void enable() {
