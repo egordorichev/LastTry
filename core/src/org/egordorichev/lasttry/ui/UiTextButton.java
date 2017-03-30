1b0728124fe36d3cc8e85356486299509da752ec
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-03-30 10:11:31 +0300

    :art: Improve UiTextButton

--- core/src/org/egordorichev/lasttry/ui/UiTextButton.java
+++ core/src/org/egordorichev/lasttry/ui/UiTextButton.java
@@ -6,41 +6,15 @@ import com.badlogic.gdx.math.Rectangle;
 import org.egordorichev.lasttry.LastTry;
 import org.egordorichev.lasttry.graphics.Fonts;
 
-public class UiTextButton extends UiComponent {
-	/** Text, to be drawn on the button */
-	protected String label;
-
-	/** Label width in pixels */
-	protected int labelWidth;
-
-	/** Current font */
-	private BitmapFont font;
-
+public class UiTextButton extends UiTextLabel {
 	public UiTextButton(Rectangle rectangle, Origin origin, String label) {
-		super(rectangle, origin);
-
-		this.label = label;
-
-		this.setFont(Fonts.f22);
+		super(rectangle, origin, label);
 	}
 
 	public UiTextButton(Rectangle rectangle, String label) {
 		this(rectangle, Origin.TOP_LEFT, label);
 	}
-
-	/** Renders the element */
-	@Override
-	public void render() {
-		if (this.hidden) {
-			return;
-		}
-
-		super.render();
-
-		this.font.draw(LastTry.batch, this.label, this.getX(),
-			this.getY() + (this.getHeight() - this.font.getLineHeight()) / 2);
-	}
-
+	
 	/** Handles state change */
 	@Override
 	protected void onStateChange() {
@@ -49,17 +23,5 @@ public class UiTextButton extends UiComponent {
 		} else if (this.state == State.NORMAL) {
 			this.setFont(Fonts.f22);
 		}
-	}
-
-	/** Sets font and calculates new size */
-	private void setFont(BitmapFont font) {
-		this.font = font;
-
-		GlyphLayout glyphLayout = new GlyphLayout();
-		glyphLayout.setText(this.font, this.label);
-
-		this.labelWidth = (int) glyphLayout.width;
-		this.rect.width = this.labelWidth;
-		this.rect.height = this.font.getLineHeight() * 1.f;
 	}
 }
