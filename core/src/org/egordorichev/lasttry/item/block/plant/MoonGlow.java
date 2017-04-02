6d22ff3844972ce8c3b5afcc1ae1ae84160ccf1b
Dorichev Egor <egordorichev@gmail.com>
2017-04-02 12:34:22 +0300

    Adding more plants

---
+++ core/src/org/egordorichev/lasttry/item/block/plant/MoonGlow.java
@@ -0,0 +1,7 @@
+package org.egordorichev.lasttry.item.block.plant;
+
+/**
+ * Created by egor on 4/2/17.
+ */
+public class MoonGlow {
+}
---
+++ core/src/org/egordorichev/lasttry/item/items/seeds/GrassSeeds.java
@@ -0,0 +1,7 @@
+package org.egordorichev.lasttry.item.items.seeds;
+
+/**
+ * Created by egor on 4/2/17.
+ */
+public class GrassSeeds {
+}
