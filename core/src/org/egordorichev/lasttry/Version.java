bf9d3ab22087bc01d42eacefafb83f1f248f5808
Egor Dorichev <egordorichev@users.noreply.github.com>
2017-04-07 08:57:41 +0300

    Changing version holder

--- core/src/org/egordorichev/lasttry/Version.java
+++ core/src/org/egordorichev/lasttry/Version.java
@@ -1,7 +1,15 @@
 package org.egordorichev.lasttry;
 
 public class Version {
-	public static final String full = "v.0.4-alpha";
-	public static final int minor = 4;
-	public static final int major = 0;
+	public int minor;
+	public int major;
+	
+	public Version(int major, int minor) {
+		this.major = major;
+		this.minor = minor;
+	}
+	
+	public String toString() {
+		return "v." + this.major + "." + this.minor;	
+	}
 }
