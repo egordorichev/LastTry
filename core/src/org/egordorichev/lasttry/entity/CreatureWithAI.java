168773158571ee0944c07571c43bb15f780cc94a
logotie <forseunakin@gmail.com>
2017-04-29 02:30:21 +0100

    Added despawn method and read lock

--- core/src/org/egordorichev/lasttry/entity/CreatureWithAI.java
+++ core/src/org/egordorichev/lasttry/entity/CreatureWithAI.java
@@ -1,6 +1,13 @@
 package org.egordorichev.lasttry.entity;
 
+import org.egordorichev.lasttry.LastTry;
 import org.egordorichev.lasttry.entity.components.*;
+import org.egordorichev.lasttry.world.WorldTime;
+import org.egordorichev.lasttry.world.spawn.components.CircleAreaComponent;
+import org.egordorichev.lasttry.world.spawn.components.GridComponent;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CreatureWithAI extends Creature {
 	public AiComponent ai = new AiComponent(this);
@@ -21,8 +28,21 @@ public class CreatureWithAI extends Creature {
 
 	public void tryToDespawn() {
 
+		try {
+			final WorldTime currentTime = LastTry.environment.time;
 
+			final CircleAreaComponent playerActiveArea = GridComponent.retrieveActiveAreaCircle(currentTime);
 
+			final boolean isEnemyInActiveArea = GridComponent.isCreatureInPlayerActiveArea(this, playerActiveArea);
+
+			if(!isEnemyInActiveArea){
+				LastTry.entityManager.markForRemoval(this);
+			}
+
+			LastTry.debug.print("Complete in try despawn");
+		} catch (Exception e){
+			LastTry.handleException(e);
+		}
 	}
 
 }
