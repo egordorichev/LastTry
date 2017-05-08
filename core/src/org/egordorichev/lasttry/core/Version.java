d37db9df24a7f841d774dd07e3287c13525811b4
Dorichev Egor <egordorichev@gmail.com>
2017-05-08 13:28:45 +0300

    Update versions

--- core/src/org/egordorichev/lasttry/core/Version.java
+++ core/src/org/egordorichev/lasttry/core/Version.java
@@ -3,14 +3,20 @@ package org.egordorichev.lasttry.core;
 public class Version {
 	public int minor;
 	public double major;
+	public String suff;
 	
-	public Version(double major, int minor) {
+	public Version(double major, int minor, String suff) {
 		this.major = major;
 		this.minor = minor;
+		this.suff = suff;
 	}
 
 	@Override
 	public String toString() {
-		return "v." + this.major + "." + this.minor;	
+		if (this.suff.isEmpty()) {
+			return "v." + this.major + "." + this.minor;
+		} else {
+			return "v." + this.major + "." + this.minor + "-" + suff;
+		}
 	}
 }
