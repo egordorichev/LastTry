27842f2d9ec107b1d65940cef3389b35869e0d7b
Egor Dorichev <egordorichev@gmail.com>
2017-05-13 12:36:43 +0300

    Update LastTry.java

--- core/src/org/egordorichev/lasttry/LastTry.java
+++ core/src/org/egordorichev/lasttry/LastTry.java
@@ -23,7 +23,7 @@ import java.util.Locale;
 /** Main game class */
 public class LastTry extends Game {
 	/** LastTry version */
-	public static final Version version = new Version(0.0, 11, "alpha");
+	public static final Version version = new Version(0.0, 12, "alpha");
 	
 	/** Random instance */
 	public static final Random random = new Random();
