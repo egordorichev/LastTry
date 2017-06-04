40a304104a9c58a3959f4d62e9bb1a040aaa88d6
Dorichev Egor <egordorichev@gmail.com>
2017-06-04 21:16:11 +0300

    Fix chat levels

--- core/src/org/egordorichev/lasttry/item/block/helpers/BlockHelper.java
+++ core/src/org/egordorichev/lasttry/item/block/helpers/BlockHelper.java
@@ -3,11 +3,11 @@ package org.egordorichev.lasttry.item.block.helpers;
 import org.egordorichev.lasttry.util.ByteHelper;
 
 public class BlockHelper {
-	public static byte getHP(byte data) {
+	public byte getHP(byte data) {
 		return ByteHelper.getSum(data, (byte) 0, (byte) 1);
 	}
 
-	public static boolean isActivated(byte data) {
+	public boolean isActivated(byte data) {
 		return ByteHelper.bitIsSet(data, (byte) 70);
 	}
 }
---
+++ core/src/org/egordorichev/lasttry/item/block/helpers/NullBlockHelper.java
@@ -0,0 +1,11 @@
+package org.egordorichev.lasttry.item.block.helpers;
+
+import org.egordorichev.lasttry.util.ByteHelper;
+
+public class NullBlockHelper extends BlockHelper {
+	public byte getHP(byte data) {
+		return 0;
+	}
+
+	// todo
+}
