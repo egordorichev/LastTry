d38d76f06485a710b4a37ee016eccd90d8a34e78
egordorichev <egordorichev@gmail.com>
2017-06-28 09:26:51 +0300

    Finish documenting input package

--- core/src/org/egordorichev/lasttry/input/DefaultInputProcessor.java
+++ core/src/org/egordorichev/lasttry/input/DefaultInputProcessor.java
@@ -2,45 +2,48 @@ package org.egordorichev.lasttry.input;
 
 import com.badlogic.gdx.InputProcessor;
 
+/**
+ * Simple implementation of InputProcessor
+ */
 public interface DefaultInputProcessor extends InputProcessor {
 	@Override
 	default boolean keyDown(int keycode) {
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    default boolean keyUp(int keycode) {
-        return false;
-    }
+	@Override
+	default boolean keyUp(int keycode) {
+		return false;
+	}
 
-    @Override
-    default boolean keyTyped(char character) {
-        return false;
-    }
+	@Override
+	default boolean keyTyped(char character) {
+		return false;
+	}
 
-    @Override
-    default boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
+	@Override
+	default boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
 
-    @Override
-    default boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
+	@Override
+	default boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
 
-    @Override
-    @Deprecated
-    default boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
+	@Override
+	@Deprecated
+	default boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
 
-    @Override
-    default boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
+	@Override
+	default boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
 
-    @Override
-    default boolean scrolled(int amount) {
-        return false;
-    }
+	@Override
+	default boolean scrolled(int amount) {
+		return false;
+	}
 }
