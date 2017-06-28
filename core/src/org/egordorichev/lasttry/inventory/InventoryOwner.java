0660c98b9db6aed9e7ed77255c24a317a2d3b440
egordorichev <egordorichev@gmail.com>
2017-06-28 11:46:35 +0300

    Finish documenting inventory package

--- core/src/org/egordorichev/lasttry/inventory/InventoryOwner.java
+++ core/src/org/egordorichev/lasttry/inventory/InventoryOwner.java
@@ -4,17 +4,17 @@ package org.egordorichev.lasttry.inventory;
  * Interface applied to creatures that hold inventories.
  */
 public interface InventoryOwner<T extends InventorySlot> {
-    /**
-     * The inventory the owner can pull up.
-     * 
-     * @return
-     */
-    Inventory<T> getInventory();
+	/**
+	 * The inventory the owner can pull up.
+	 *
+	 * @return
+	 */
+	Inventory<T> getInventory();
 
-    /**
-     * Sets the entity's inventory.
-     * 
-     * @param inventory
-     */
-    void setInventory(Inventory<T> inventory);
+	/**
+	 * Sets the entity's inventory.
+	 *
+	 * @param inventory
+	 */
+	void setInventory(Inventory<T> inventory);
 }
--- core/src/org/egordorichev/lasttry/inventory/ItemHolder.java
+++ core/src/org/egordorichev/lasttry/inventory/ItemHolder.java
@@ -7,9 +7,21 @@ import org.egordorichev.lasttry.item.Item;
 import org.egordorichev.lasttry.item.modifier.Modifier;
 import org.egordorichev.lasttry.util.Util;
 
+/**
+ * Class, that holds item, count and item modifier
+ */
 public class ItemHolder {
+	/**
+	 * Item count
+	 */
 	private int count;
+	/**
+	 * Item pointer
+	 */
 	private Item item;
+	/**
+	 * Item modifier
+	 */
 	private Modifier modifier;
 
 	public ItemHolder(Item item, int count, Modifier modifier) {
@@ -22,6 +34,12 @@ public class ItemHolder {
 		this(item, count, null);
 	}
 
+	/**
+	 * Renders item at given position
+	 *
+	 * @param x Render X
+	 * @param y Render Y
+	 */
 	public void renderAt(int x, int y) {
 		if (this.item != null) {
 			TextureRegion texture = this.item.getTextureRegion();
@@ -31,11 +49,19 @@ public class ItemHolder {
 			Graphics.batch.draw(texture, x, y);
 
 			if (this.count > 1) {
-				Util.drawWithShadow(Assets.f18,  String.format("%d", this.count), x - 8, y + th - 8);
+				Util.drawWithShadow(Assets.f18, String.format("%d", this.count), x - 8, y + th - 8);
 			}
 		}
 	}
 
+	/**
+	 * Renders item at given position and with given size
+	 *
+	 * @param x Render X
+	 * @param y Render Y
+	 * @param width Item width
+	 * @param height Item height
+	 */
 	public void renderAt(int x, int y, int width, int height) {
 		if (this.item != null) {
 			TextureRegion texture = this.item.getTextureRegion();
@@ -47,15 +73,25 @@ public class ItemHolder {
 			Graphics.batch.draw(texture, x + (width - tw) / 2, iy);
 
 			if (this.count > 1) {
-				Util.drawWithShadow(Assets.f18,  String.format("%d", this.count), x + tw / 2, iy + th / 2);
+				Util.drawWithShadow(Assets.f18, String.format("%d", this.count), x + tw / 2, iy + th / 2);
 			}
 		}
 	}
 
+	/**
+	 * Sets item
+	 *
+	 * @param item New item
+	 */
 	public void setItem(Item item) {
 		this.item = item;
 	}
 
+	/**
+	 * Sets count
+	 *
+	 * @param count New count
+	 */
 	public void setCount(int count) {
 		if (this.item == null) {
 			this.count = 0;
@@ -69,26 +105,46 @@ public class ItemHolder {
 		this.count = count;
 	}
 
+	/**
+	 * Sets modifier
+	 *
+	 * @param modifier New modifier
+	 */
 	public void setModifier(Modifier modifier) {
 		this.modifier = modifier;
 	}
 
+	/**
+	 * @return Item count
+	 */
 	public int getCount() {
 		return this.count;
 	}
 
+	/**
+	 * @return Item pointer
+	 */
 	public Item getItem() {
 		return this.item;
 	}
 
+	/**
+	 * @return Item modifier
+	 */
 	public Modifier getModifier() {
 		return this.modifier;
 	}
 
+	/**
+	 * @return Holder is empty
+	 */
 	public boolean isEmpty() {
 		return this.item == null || this.count == 0;
 	}
 
+	/**
+	 * @return String info about item
+	 */
 	public String asInfo() {
 		if (this.isEmpty()) {
 			return "";
@@ -103,7 +159,7 @@ public class ItemHolder {
 		info += this.getItem().getName();
 
 		if (this.getCount() != 1) {
-			info += " (" +  this.getCount() + ")";
+			info += " (" + this.getCount() + ")";
 		}
 
 		return info;
