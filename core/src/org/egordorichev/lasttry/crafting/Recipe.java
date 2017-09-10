fcb3d3ec921fe4d5785dba7f89844299c165fa32
Matt <Col-E@users.noreply.github.com>
2017-09-10 01:46:19 -0400

    Revert "Small fix/crafting recipe holder constructor capsulation"

--- core/src/org/egordorichev/lasttry/crafting/Recipe.java
+++ core/src/org/egordorichev/lasttry/crafting/Recipe.java
@@ -3,53 +3,23 @@ package org.egordorichev.lasttry.crafting;
 import com.badlogic.gdx.utils.JsonValue;
 
 public class Recipe {
-	
 	/**
-	 * Required crafting stations.
+	 * Required crafting stations
 	 */
 	private String[] stations;
-	
 	/**
-	 * Required materials.
+	 * Required materials
 	 */
 	private Holder[] materials;
-	
 	/**
-	 * Result of crafting.
+	 * Result of crafting
 	 */
 	private Holder result;
 
-	private static class Holder {
-
-		/**
-		 * Item name.
-		 */
-		private String item;
-
-		/**
-		 * Amount required.
-		 */
-		private short count;
-
-		/**
-		 * Class constructor.
-		 * @param item 
-		 * @param count 
-		 */
-		Holder(String item, short count) {
-			this.item = item;
-			this.count = count;
-		}
-
-		public String getItem() {
-			return item;
-		}
-
-		public short getCount() {
-			return count;
-		}
+	public static class Holder {
+		public String item;
+		public short count;
 	}
-
 
 	public Recipe() {
 
@@ -57,22 +27,12 @@ public class Recipe {
 
 	/**
 	 * Loads recipe from json
-	 *
 	 * @param root Json root
 	 * @throws Exception Exception containing parse error
 	 */
-	public static Recipe load(JsonValue root) throws Exception {
-		Recipe recipe = new Recipe();
-
+	public void load(JsonValue root) throws Exception {
 		if (root.has("stations")) {
-			JsonValue stations = root.get("stations");
-			recipe.stations = new String[stations.size];
 
-			for (int i = 0; i < stations.size; i++) {
-				recipe.stations[i] = stations.getString(i);
-			}
-		} else {
-			recipe.stations = new String[0];
 		}
 
 		if (!root.has("materials") || !root.has("result")) {
@@ -80,38 +40,18 @@ public class Recipe {
 		}
 
 		JsonValue materials = root.get("materials");
-		recipe.materials = new Holder[materials.size];
+		this.materials = new Holder[materials.size];
 
 		for (int i = 0; i < materials.size; i++) {
-			recipe.materials[i] = new Holder(materials.get(i).getString("id", "lt:dirt"), materials.get(i).getShort("count", (short) 1));
-			
+			this.materials[i] = new Holder();
+			this.materials[i].count = root.get(i).getShort("count", (short) 1);
+			this.materials[i].item = root.get(i).getString("id", "lt:dirt"); // ;D
 		}
 
 		JsonValue result = root.get("result");
 
-		recipe.result = new Holder(result.getString("id", "lt:dirt"), result.getShort("count", (short) 1));
-		
-		return recipe;
-	}
-
-	/**
-	 * @return Recipe result
-	 */
-	public Holder getResult() {
-		return this.result;
-	}
-
-	/**
-	 * @return Recipe required materials
-	 */
-	public Holder[] getMaterials() {
-		return this.materials;
-	}
-
-	/**
-	 * @return Recipe required stations
-	 */
-	public String[] getStations() {
-		return this.stations;
+		this.result = new Holder();
+		this.result.count = result.getShort("count", (short) 1);
+		this.result.item = result.getString("id", "lt:dirt"); // ;D
 	}
 }
