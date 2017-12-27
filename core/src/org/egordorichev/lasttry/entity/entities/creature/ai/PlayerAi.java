71cdaac5ed6e3e523927c9a39590d1613c57eef9
angelickite <>
2017-12-27 16:41:17 +0100

    Begin work on player movement behaviour. Less Moony.

--- core/src/org/egordorichev/lasttry/entity/entities/creature/ai/PlayerAi.java
+++ core/src/org/egordorichev/lasttry/entity/entities/creature/ai/PlayerAi.java
@@ -17,9 +17,44 @@ import org.egordorichev.lasttry.util.math.NumberUtil;
  * Player AI
  */
 public class PlayerAi extends Ai {
-	public static float JUMP_ACCELERATION = 7f;
-	public static float FLY_ACCELERATION = 0.2f;
-	public static float MOVE_ACCELERATION = 0.8f;
+	/**
+	 * How strongly the player is pulled down when in the air. 
+	 */
+	private static final float GRAVITY_FORCE = -.04f;
+	/**
+	 * Maximum vertical speed.
+	 */
+	private static final float VERTICAL_SPEED_MAX = 10f;
+	/**
+	 * How strongly the player jumps. Competes with gravity.
+	 */
+	private static final float JUMP_FORCE = 5f;
+	/**
+	 * How strongly the player moves horizontally when in the air.
+	 */
+	private static final float AIR_MOVE_FORCE_X = .6f;
+	/**
+	 * Maximum horizontal speed when in the air.
+	 */
+	private static final float AIR_MOVE_SPEED_MAX = 1.2f;
+	/**
+	 * How much horizontal resistance the air has. Sensible values in range: [0, 1]
+	 * Lower means less resistance.
+	 */
+	private static final float AIR_FRICTION = .3f;
+	/**
+	 * How strongly the player moves horizontally when on the ground.
+	 */
+	private static final float GROUND_MOVE_FORCE_X = .7f;
+	/**
+	 * Maximum horizontal speed when on the ground.
+	 */
+	private static final float GROUND_MOVE_SPEED_MAX = 1.3f;
+	/**
+	 * How much horizontal resistance the ground has. Sensible values in range: [0, 1]
+	 * Lower means less resistance.
+	 */
+	private static final float GROUND_FRICTION = .3f;
 
 	public PlayerAi() {
 		this.registerAll("idle", "walking", "flying");
@@ -27,11 +62,10 @@ public class PlayerAi extends Ai {
 
 	public void idle(Creature self, int t) {
 		AccelerationComponent acceleration = self.getComponent(AccelerationComponent.class);
-		VelocityComponent velocity = self.getComponent(VelocityComponent.class);
 		CollisionComponent collision = self.getComponent(CollisionComponent.class);
 
 		if (collision.onGround && Gdx.input.isKeyPressed(Assets.keys.get("jump"))) {
-			acceleration.y += JUMP_ACCELERATION;
+			acceleration.y += JUMP_FORCE;
 			self.become("flying");
 			return;
 		}
@@ -39,10 +73,10 @@ public class PlayerAi extends Ai {
 		if (Gdx.input.isKeyPressed(Assets.keys.get("move_left"))
 			|| Gdx.input.isKeyPressed(Assets.keys.get("move_right"))) {
 			self.become("walking");
+			return;
 		}
 
-		velocity.x *= 0.93;
-		velocity.y *= 0.93;
+		acceleration.y += GRAVITY_FORCE;
 	}
 
 	public void walking(Creature self, int t) {
@@ -51,27 +85,28 @@ public class PlayerAi extends Ai {
 		CollisionComponent collision = self.getComponent(CollisionComponent.class);
 
 		if (collision.onGround && Gdx.input.isKeyPressed(Assets.keys.get("jump"))) {
-			acceleration.y += JUMP_ACCELERATION;
+			acceleration.y += JUMP_FORCE;
 			self.become("flying");
 			return;
 		}
 
-		if (Math.abs(velocity.x) < 0.5) {
+		if (Math.abs(velocity.x) < 0.01f) {
 			velocity.x = 0;
 			self.become("idle");
 		}
 
 		if (Gdx.input.isKeyPressed(Assets.keys.get("move_left"))) {
-			acceleration.x -= MOVE_ACCELERATION;
+			acceleration.x -= GROUND_MOVE_FORCE_X;
 		}
 
 		if (Gdx.input.isKeyPressed(Assets.keys.get("move_right"))) {
-			acceleration.x += MOVE_ACCELERATION;
+			acceleration.x += GROUND_MOVE_FORCE_X;
 		}
 
-		velocity.x *= 0.93;
-		velocity.y *= 0.93;
-		velocity.x = NumberUtil.clamp(velocity.x, -3, 3);
+		velocity.x *= (1f - GROUND_FRICTION);
+		velocity.x = NumberUtil.clamp(velocity.x, -GROUND_MOVE_SPEED_MAX, GROUND_MOVE_SPEED_MAX);
+
+		acceleration.y += GRAVITY_FORCE;
 	}
 
 	public void flying(Creature self, int t) {
@@ -80,24 +115,21 @@ public class PlayerAi extends Ai {
 		CollisionComponent collision = self.getComponent(CollisionComponent.class);
 
 		if (collision.onGround) {
-			self.become("idle");
+			self.become("walking");
 			return;
+		} else {
+			if (Gdx.input.isKeyPressed(Assets.keys.get("move_left"))) {
+				acceleration.x -= AIR_MOVE_FORCE_X;
+			} else if (Gdx.input.isKeyPressed(Assets.keys.get("move_right"))) {
+				acceleration.x += AIR_MOVE_FORCE_X;
+			}
 		}
 
-		if (Gdx.input.isKeyPressed(Assets.keys.get("move_left"))) {
-			acceleration.x -= FLY_ACCELERATION;
-			self.become("walking");
-		}
+		velocity.x *= (1f - AIR_FRICTION);
+		velocity.x = NumberUtil.clamp(velocity.x, -AIR_MOVE_SPEED_MAX, AIR_MOVE_SPEED_MAX);
 
-		if (Gdx.input.isKeyPressed(Assets.keys.get("move_right"))) {
-			acceleration.x += FLY_ACCELERATION;
-			self.become("walking");
-		}
-
-		velocity.x *= 0.93;
-		velocity.y *= 0.93;
-
-		velocity.y = NumberUtil.clamp(velocity.y, -10, 10);
+		acceleration.y += GRAVITY_FORCE;
+		velocity.y = NumberUtil.clamp(velocity.y, -VERTICAL_SPEED_MAX, VERTICAL_SPEED_MAX);
 	}
 
 	@Override
